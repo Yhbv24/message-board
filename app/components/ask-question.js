3baecd418176f289b8bc7cb35ebb474abc69d023
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-03-24 09:59:50 -0700

    Add a form to ask questions, which routes back to the index

--- app/components/ask-question.js
+++ app/components/ask-question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    askQuestion() {
+      var params = {
+        title: this.get("title"),
+        body: this.get("body"),
+        author: this.get("author")
+      }
+      this.sendAction("askQuestion", params);
+    }
+  }
 });
